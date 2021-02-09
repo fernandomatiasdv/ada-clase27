c8cff3223e1e78de6d073dedb135c849043a3471
Fernando del Valle <fernando.delvalle@outsidecube.com>
2021-02-09 05:19:41 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,92 @@
+/*
+let gusto1 = prompt("Ingrese el primer gusto");
+let gusto2 = prompt("Ingrese el segundo gusto");
+let gusto3 = prompt("Ingrese el tercer gusto");
+
+console.log("Aquí tiene ${} su helado de "+ gusto1+", " + gusto2+" y " + gusto3);
+console.log(`Aquí tiene su helado de ${gusto1}, ${gusto2} y ${gusto3}`);
+
+console.log(`Aquí tiene \${} su helado de ${gusto1}, ${gusto2} y ${gusto3}`);
+
+alert(`Aquí tiene su helado de ${gusto1}, ${gusto2} y ${gusto3}`)
+
+let edad = prompt("Decinos tu edad");
+const edad_perro = 8;
+
+let edad_en_perro = Number(edad) * edad_perro;
+
+console.log(`Tu edad en perro es ${edad_en_perro}`);
+console.log(`Tu edad en perro es ${Number(edad) * edad_perro}`);
+
+*/
+
+/*
+function saludo(nombre, edad=30){
+    console.log(`su edad es ${edad} y su nombre ${nombre}.`);
+};
+
+saludo("Adriana", 20);
+saludo("Juan");
+saludo("Martin", 30);
+
+Realizar una función que se llame "Presentacion". 
+Le enviaremos dos parametros:
+1) nombre
+2) edad
+Y me va a mostrar por alert:
+ => Su nombre es ____ y su edad es _______
+
+*/
+/*
+function retorna8(){
+    return 8 ;
+}
+
+//console.log( 
+let numero = retorna(); 
+  //  )
+
+
+*/
+
+
+
+
+
+/*
+function sumar(parametro1, parametro2){
+
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+const sumar = (parametro1, parametro2) => {
+    let suma = Number(parametro1) + Number(parametro2);
+    return suma;
+}
+
+let valorsumado =sumar(20,3);
+console.log(valorsumado);
+let valorsumado2 =sumar(1,6);
+console.log(valorsumado2);
+
+
+
+
+
+
+
+
+
+
+
